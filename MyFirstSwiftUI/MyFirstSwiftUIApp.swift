094af49b14805996d087633a713bfb545ca2c3fe
Andreas <dablnazero@gmail.com>
2022-04-16 18:46:51 +0300

    Initial Commit

---
+++ MyFirstSwiftUI/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  MyFirstSwiftUI
+//
+//  Created by Andreas on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
